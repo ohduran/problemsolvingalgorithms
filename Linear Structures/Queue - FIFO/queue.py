0698a60b55d63b82a22de4d6ae679b99dae17c1d
ohduran <alvaro.duranb@gmail.com>
2018-05-01 06:11:35 +0100

    Queue data structured implemented and tested

--- Linear Structures/Queue - FIFO/queue.py
+++ Linear Structures/Queue - FIFO/queue.py
@@ -4,4 +4,24 @@
 class Queue():
     """Queue data structure - FIFO."""
 
-    pass
+    def __init__(self):
+        """Define queue - FIFO."""
+        self._items = []
+
+    def is_empty(self):
+        """Return True if the queue is empty."""
+        return len(self._items) == 0
+
+    def enqueue(self, item):
+        """Add a new item to the end of the queue."""
+        self._items.append(item)
+
+    def dequeue(self):
+        """Remove the first item of the queue and return it."""
+        first = self._items[0]
+        self._items.remove(first)
+        return first
+
+    def size(self):
+        """Return the size of the queue."""
+        return len(self._items)
--- Linear Structures/Queue - FIFO/test-queue.py
+++ Linear Structures/Queue - FIFO/test-queue.py
@@ -1,10 +1,46 @@
 import unittest
+from queue import Queue
 
 
 class TestQueue(unittest.TestCase):
     """Test Queue class."""
 
-    pass
+    def test_enqueue(self):
+        """Enqueue must add a new item at the end of the queue."""
+        q = Queue()
+        self.assertEqual(len(q._items), 0)
+        q.enqueue('hello')
+        self.assertEqual(q._items, ['hello'])
+
+    def test_is_empty(self):
+        """Test is_empty method."""
+        q = Queue()
+        self.assertTrue(q.is_empty())
+        q._items.append('hello')
+        self.assertFalse(q.is_empty())
+
+    def test_dequeue(self):
+        """
+        dequeue must return the first item on the list.
+        And remove it from the list.
+        """
+        q = Queue()
+        q._items.append('hello')
+        q._items.append('world')
+        q._items.append('hello')
+        q.dequeue()
+        self.assertEqual(q._items, ['world', 'hello'])
+
+    def test_size(self):
+        """Size must return length of the queue."""
+        q = Queue()
+        q._items.append('hello')
+        q._items.append('world')
+        q._items.append('hello')
+        q._items.append('world')
+        self.assertEqual(q.size(), 4)
+
+
 
 
 if __name__ == '__main__':
