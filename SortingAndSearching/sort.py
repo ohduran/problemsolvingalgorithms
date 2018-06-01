6fafc70d10fc8d9d93aaab422ebd2ec21d7894e7
ohduran <alvaro.duranb@gmail.com>
2018-06-01 15:16:19 +0100

    Redo tests and include selection sort

--- SortingAndSearching/sort.py
+++ SortingAndSearching/sort.py
@@ -2,9 +2,20 @@
 
 
 def bubble_sort(a_list):
-    """Bubble Sort algorithm."""
+    """Apply Bubble Sort algorithm."""
     for item in reversed(range(len(a_list))):
         for i in range(item):
             if a_list[i] > a_list[i + 1]:
                 a_list[i], a_list[i + 1] = a_list[i + 1], a_list[i]
+    return a_list
+
+
+def selection_sort(a_list):
+    """Apply Selection Sort algorithm."""
+    for slot in reversed(range(len(a_list))):
+        pos_of_max = 0
+        for location in range(1, slot + 1):
+            if a_list[location] > a_list[pos_of_max]:
+                pos_of_max = location
+        a_list[slot], a_list[pos_of_max] = a_list[pos_of_max], a_list[slot]
     return a_list
--- SortingAndSearching/test_sort.py
+++ SortingAndSearching/test_sort.py
@@ -6,14 +6,24 @@ unsorted_list = [3, 4, 1, 7, 2, 5, 9, 8, 6]
 sorted_list = range(1, 10)
 
 
+def verify_sort(self, sort_function):
+    """Verification test for a sort function."""
+    result = sort_function(unsorted_list)
+    self.assertEqual(result, sorted_list)
+
+
 class TestSort(unittest.TestCase):
     """Test Sort algorithms."""
 
-    def test_bubble_sort(self):
-        """Test bubble_sort."""
-        result = sort.bubble_sort(unsorted_list)
+    def test_all_sorts(self):
+        """Iterate over all functions and assert that everything works."""
+        list_of_functions = [
+            sort.bubble_sort,
+            sort.selection_sort,
+        ]
 
-        self.assertEqual(result, sorted_list)
+        for sort_function in list_of_functions:
+            verify_sort(self, sort_function)
 
 
 if __name__ == '__main__':
