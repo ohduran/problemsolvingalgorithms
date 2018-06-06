22126c03d9bc09fd5754ce4d336f45b564e6d026
ohduran <alvaro.duranb@gmail.com>
2018-06-06 07:20:53 +0100

    Binary Tree skeleton

--- Trees and Tree Algorithms/trees.py
+++ Trees and Tree Algorithms/trees.py
@@ -2,4 +2,28 @@
 
 
 class BinaryTree():
-    pass
+    """A Binary Tree is a Tree with a maximum of two children per node."""
+
+    def get_left_child(self, node):
+        """Return the binary tree corresponding to the left child of node."""
+        pass
+
+    def get_right_child(self, node):
+        """Return the binary tree corresponding to the right child of node."""
+        pass
+
+    def set_root_value(self, value, node):
+        """Store the object value in node."""
+        pass
+
+    def get_root_value(self, node):
+        """Return the object stored in node."""
+        pass
+
+    def insert_left(self, value, node):
+        """Create a new Binary Tree and install as the left child of node."""
+        pass
+
+    def insert_right(self, value, node):
+        """Create a new Binary Tree and install as the right child of node."""
+        pass
