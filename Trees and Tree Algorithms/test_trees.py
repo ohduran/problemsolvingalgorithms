c9f2a0270f0f0e7511458c257bbea8e4a5992885
ohduran <alvaro.duranb@gmail.com>
2018-06-06 07:41:30 +0100

    Binary Tree implemented and tested

--- Trees and Tree Algorithms/test_trees.py
+++ Trees and Tree Algorithms/test_trees.py
@@ -1,10 +1,38 @@
 import unittest
+import trees
 
 
 class TestTrees(unittest.TestCase):
     """Test Trees."""
 
-    pass
+    def test_binary_tree_root_methods(self):
+        """Test the initial behaviour of the Binary Tree."""
+        r = trees.BinaryTree('a')
+        self.assertEqual(r.get_root_value(), 'a')
+
+        r.set_root_value('hello')
+        self.assertEqual(r.get_root_value(), 'hello')
+
+    def test_binary_tree_left_methods(self):
+        """Test left child functionality."""
+        r = trees.BinaryTree('a')
+        self.assertIsNone(r.get_left_child())
+
+        r.insert_left('b')
+        self.assertTrue(isinstance(r.get_left_child(), trees.BinaryTree))
+        self.assertEqual(r.get_left_child().get_root_value(), 'b')
+
+    def test_binary_tree_right_methods(self):
+        """Test right child functionality."""
+        r = trees.BinaryTree('a')
+        self.assertIsNone(r.get_right_child())
+
+        r.insert_right('c')
+        self.assertTrue(isinstance(r.get_right_child(), trees.BinaryTree))
+        self.assertEqual(r.get_right_child().get_root_value(), 'c')
+
+
+
 
 
 class TestTreeAlgorithms(unittest.TestCase):
--- Trees and Tree Algorithms/trees.py
+++ Trees and Tree Algorithms/trees.py
@@ -4,26 +4,50 @@
 class BinaryTree():
     """A Binary Tree is a Tree with a maximum of two children per node."""
 
-    def get_left_child(self, node):
+    def __init__(self, key):
+        """Instantiate a Binary Tree."""
+        self.key = key
+        self.left_child = None
+        self.right_child = None
+
+    def get_left_child(self):
         """Return the binary tree corresponding to the left child of node."""
-        pass
+        return self.left_child
 
-    def get_right_child(self, node):
+    def get_right_child(self):
         """Return the binary tree corresponding to the right child of node."""
-        pass
+        return self.right_child
 
-    def set_root_value(self, value, node):
-        """Store the object value in node."""
-        pass
+    def set_root_value(self, value):
+        """Store the object value in root."""
+        self.key = value
 
-    def get_root_value(self, node):
+    def get_root_value(self):
         """Return the object stored in node."""
-        pass
+        return self.key
 
-    def insert_left(self, value, node):
-        """Create a new Binary Tree and install as the left child of node."""
-        pass
+    def insert_left(self, node):
+        """
+        Create a new Binary Tree and install as the left child of node.
+        If a left child already exists,
+        it will become the left child of the new node.
+        """
+        if self.left_child is None:
+            self.left_child = BinaryTree(node)
+        else:
+            bt = BinaryTree(node)
+            bt.left_child = self.left_child
+            self.left_child = bt
 
-    def insert_right(self, value, node):
-        """Create a new Binary Tree and install as the right child of node."""
-        pass
+    def insert_right(self, node):
+        """
+        Create a new Binary Tree and install as the right child of node.
+        If a right child already exists,
+        it will become the right child of the new node.
+        """
+        if self.right_child is None:
+            self.right_child = BinaryTree(node)
+        else:
+            bt = BinaryTree(node)
+            bt.right_child = self.right_child
+            self.right_child = bt
